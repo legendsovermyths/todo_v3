226cc4b9744025e3aa277e6dbfedf38f4d75412d
Mask Wolf <maskwolf2000@gmail.com>
2020-07-24 09:33:29 +0530

    add basic function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 
 function App() {
+  let [listOfItems,editListOfItems]=useState(["An Item"])
+  var item;
+  function handelChange(events){
+    item=events.target.value;
+  }
+  function handelClick(){
+       editListOfItems(
+         [...listOfItems,item]
+      )
+  }
+  function makeList(item){
+    return <li>{item}</li>
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handelChange} type="text" />
+        <button onClick={handelClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {listOfItems.map(makeList)}
         </ul>
       </div>
     </div>
